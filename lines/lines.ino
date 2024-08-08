268f9233a2dffa893e52eec520b8bad2c62dab75
Martin Stannard <martin@stannard.id.au>
2024-08-09 08:39:23 +1000

    make lines move around the sides

--- lines/lines.ino
+++ lines/lines.ino
@@ -6,6 +6,8 @@
 #include <LittleVector.h>
 
 #define DATA_PIN 9
+#define DELAY 10
+#define FADE 220
 
 // Define the array of leds
 #define NUM_LEDS 256
@@ -32,18 +34,25 @@ void setup() {
 }
 
 void loop() {
-  fadeall(250);
-  Line line = bresenhamLine(random8(16), random8(16), random8(16), random8(16));
-  if (random(10) == 0) {drawline(line);}
+  // leds[XYsafe(0, 0)] = CHSV(255, 255, 255);
+  // leds[XYsafe(15, 0)] = CHSV(200, 255, 255);
+  // leds[XYsafe(15, 15)] = CHSV(FADE, 255, 255);
+  // leds[XYsafe(0, 15)] = CHSV(70, 255, 255);
+
+  side1();
+  side2();
+  side3();
+  side4();
+  side5();
+  side6();
+  side7();
+  side8();
   FastLED.show();
-
-  delay(10);
-
   // display_freeram();
 }
 
 void drawline(Line points) {
-  uint8_t hue = random8(255);
+  uint8_t hue = 222;
   for (auto point : points) {
     leds[XYsafe(point.x, point.y)] = CHSV(hue, 255, 255);
   }
@@ -88,4 +97,85 @@ Line bresenhamLine(int x0, int y0, int x1, int y1) {
 
 void fadeall(uint8_t scale) {
   for (int i = 0; i < NUM_LEDS; i++) { leds[i].nscale8(scale); }
+}
+
+void side1() {
+  for (int x = 0; x < 16; x++) {
+    fadeall(FADE);
+    Line line = bresenhamLine(x, 15, 0, 0);
+    drawline(line);
+    FastLED.show();
+    delay(DELAY);
+  }
+}
+
+void side2() {
+  for (int y = 0; y < 16; y++) {
+    fadeall(FADE);
+    Line line = bresenhamLine(0, y, 15, 15);
+    drawline(line);
+    FastLED.show();
+    delay(DELAY);
+  }
+}
+
+void side3() {
+  for (int y = 15; y > 0; y--) {
+    fadeall(FADE);
+    Line line = bresenhamLine(0, 15, 15, y);
+    drawline(line);
+    FastLED.show();
+    delay(DELAY);
+  }
+}
+
+void side4() {
+  for (int x = 0; x < 15; x++) {
+    fadeall(FADE);
+    Line line = bresenhamLine(x, 15, 15, 0);
+    drawline(line);
+    FastLED.show();
+    delay(DELAY);
+  }
+}
+
+void side5() {
+  for (int x = 15; x > 0; x--) {
+    fadeall(FADE);
+    Line line = bresenhamLine(15, 15, x, 0);
+    drawline(line);
+    FastLED.show();
+    delay(DELAY);
+  }
+}
+
+
+void side6() {
+  for (int y = 15; y > 0; y--) {
+    fadeall(FADE);
+    Line line = bresenhamLine(15, y, 0, 0);
+    drawline(line);
+    FastLED.show();
+    delay(DELAY);
+  }
+}
+
+void side7() {
+  for (int y = 0; y < 16; y++) {
+    fadeall(FADE);
+    Line line = bresenhamLine(0, y, 15, 0);
+    drawline(line);
+    FastLED.show();
+    delay(DELAY);
+  }
+}
+
+void side8() {
+  for (int x = 15; x > 0; x--) {
+    fadeall(FADE);
+    Line line = bresenhamLine(0, 15, x, 0);
+    drawline(line);
+    FastLED.show();
+    delay(DELAY);
+  }
 }
